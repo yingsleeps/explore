fcb3112b53a9c180fe4cd1f4304263d069eaa180
lawtlee <lawsac2947@gmail.com>
2024-04-21 04:36:17 -0700

    Home is now fixed

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -47,42 +47,43 @@ export default function HomeScreen() {
     const [lockModeEnabled, setLockModeEnabled] = useState(true);
     const [modalVisible, setModalVisible] = useState(false);
     const [currentQuest, setCurrentQuest] = useState('');
-//     useEffect(() => {
-//         let lastPosition = { latitude: null, longitude: null };
 
-//         const watchId = Geolocation.watchPosition(
-//             (pos) => {
-//                 const { latitude, longitude } = pos.coords;
-//                 const newPosition = {latitude, longitude};
-//                 const newRectangle = getRectanglePoints({ latitude, longitude });
+    useEffect(() => {
+        let lastPosition = { latitude: null, longitude: null };
 
-//                 // if (getDistance(lastPosition.latitude, lastPosition.longitude, latitude, longitude) > 2) { // meters
-//                 //     const newHeading = lastPosition.latitude ? calculateBearing(
-//                 //         lastPosition.latitude,
-//                 //         lastPosition.longitude,
-//                 //         latitude,
-//                 //         longitude
-//                 //     ) : camera.heading;
+        const watchId = Geolocation.watchPosition(
+            (pos) => {
+                const { latitude, longitude } = pos.coords;
+                const newPosition = {latitude, longitude};
+                const newRectangle = getRectanglePoints({ latitude, longitude });
+
+                // if (getDistance(lastPosition.latitude, lastPosition.longitude, latitude, longitude) > 2) { // meters
+                //     const newHeading = lastPosition.latitude ? calculateBearing(
+                //         lastPosition.latitude,
+                //         lastPosition.longitude,
+                //         latitude,
+                //         longitude
+                //     ) : camera.heading;
     
-//                 //     const newCamera = {
-//                 //         ...camera,
-//                 //         center: newPosition,
-//                 //         heading: newHeading  // maintain the old heading initially
-//                 //     };
-//                 //     setCamera(newCamera);
+                //     const newCamera = {
+                //         ...camera,
+                //         center: newPosition,
+                //         heading: newHeading  // maintain the old heading initially
+                //     };
+                //     setCamera(newCamera);
 
-//                 //     // Animate camera smoothly to the new position and heading
-//                 //     if (mapRef.current) {
-//                 //         mapRef.current.animateCamera(newCamera, { duration: 1000 });
-//                 //     }
-//                 // }
+                //     // Animate camera smoothly to the new position and heading
+                //     if (mapRef.current) {
+                //         mapRef.current.animateCamera(newCamera, { duration: 1000 });
+                //     }
+                // }
 
-//                 // if (camera.pitch != 45) {
-//                 //     setCamera(prevCamera => ({
-//                 //         ...prevCamera,
-//                 //         pitch: 45
-//                 //     }));
-//                 // }
+                // if (camera.pitch != 45) {
+                //     setCamera(prevCamera => ({
+                //         ...prevCamera,
+                //         pitch: 45
+                //     }));
+                // }
 
                 if (lockModeEnabled) {
                     const newHeading = lastPosition.latitude ? calculateBearing(
@@ -117,8 +118,7 @@ export default function HomeScreen() {
                 //     }]))
                 //     .catch(error => console.error('Error fetching place details:', error));
                 
-//                 // setPlaces([...places, {latitude, longitude}])
-
+                // setPlaces([...places, {latitude, longitude}])
 
                 if (isPointOutsideHull(newPosition, hullPoints)) {
                     const newPoints = [...points, ...newRectangle];
@@ -135,62 +135,62 @@ export default function HomeScreen() {
         return () => Geolocation.clearWatch(watchId);
     }, [hullPoints, lockModeEnabled]);
 
-//     function calculateBearing(startLat, startLng, destLat, destLng) {
-//         startLat = degreesToRadians(startLat);
-//         startLng = degreesToRadians(startLng);
-//         destLat = degreesToRadians(destLat);
-//         destLng = degreesToRadians(destLng);
+    function calculateBearing(startLat, startLng, destLat, destLng) {
+        startLat = degreesToRadians(startLat);
+        startLng = degreesToRadians(startLng);
+        destLat = degreesToRadians(destLat);
+        destLng = degreesToRadians(destLng);
     
-//         const y = Math.sin(destLng - startLng) * Math.cos(destLat);
-//         const x = Math.cos(startLat) * Math.sin(destLat) -
-//                   Math.sin(startLat) * Math.cos(destLat) * Math.cos(destLng - startLng);
-//         const bearing = Math.atan2(y, x);
-//         return radiansToDegrees(bearing);
-//     }
+        const y = Math.sin(destLng - startLng) * Math.cos(destLat);
+        const x = Math.cos(startLat) * Math.sin(destLat) -
+                  Math.sin(startLat) * Math.cos(destLat) * Math.cos(destLng - startLng);
+        const bearing = Math.atan2(y, x);
+        return radiansToDegrees(bearing);
+    }
     
-//     function degreesToRadians(degrees) {
-//         return degrees * (Math.PI / 180);
-//     }
+    function degreesToRadians(degrees) {
+        return degrees * (Math.PI / 180);
+    }
     
-//     function radiansToDegrees(radians) {
-//         return radians * (180 / Math.PI);
-//     }
+    function radiansToDegrees(radians) {
+        return radians * (180 / Math.PI);
+    }
 
-//     // Returns rectangle buffer around location
-//     function getRectanglePoints(center, size = 0.0005) {
-//         const { latitude, longitude } = center;
-//         return [
-//             { latitude: latitude + size, longitude: longitude + size },
-//             { latitude: latitude - size, longitude: longitude + size },
-//             { latitude: latitude - size, longitude: longitude - size },
-//             { latitude: latitude + size, longitude: longitude - size },
-//         ];
-//     }
+    // Returns rectangle buffer around location
+    function getRectanglePoints(center, size = 0.0005) {
+        const { latitude, longitude } = center;
+        return [
+            { latitude: latitude + size, longitude: longitude + size },
+            { latitude: latitude - size, longitude: longitude + size },
+            { latitude: latitude - size, longitude: longitude - size },
+            { latitude: latitude + size, longitude: longitude - size },
+        ];
+    }
 
-//     // Checks if current location is outside visited polygon
-//     function isPointOutsideHull(point, hullPoints) {
-//         const x = point.longitude, y = point.latitude;
-//         let isInside = false;
-//         for (let i = 0, j = hullPoints.length - 1; i < hullPoints.length; j = i++) {
-//             const xi = hullPoints[i].longitude, yi = hullPoints[i].latitude;
-//             const xj = hullPoints[j].longitude, yj = hullPoints[j].latitude;
+    // Checks if current location is outside visited polygon
+    function isPointOutsideHull(point, hullPoints) {
+        const x = point.longitude, y = point.latitude;
+        let isInside = false;
+        for (let i = 0, j = hullPoints.length - 1; i < hullPoints.length; j = i++) {
+            const xi = hullPoints[i].longitude, yi = hullPoints[i].latitude;
+            const xj = hullPoints[j].longitude, yj = hullPoints[j].latitude;
     
-//             const intersect = ((yi > y) !== (yj > y))
-//                 && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
-//             if (intersect) isInside = !isInside;
-//         }
-//         return !isInside;
-//     }
+            const intersect = ((yi > y) !== (yj > y))
+                && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+            if (intersect) isInside = !isInside;
+        }
+        return !isInside;
+    }
     
-//     // Updates visited polygon
-//     function updateHull(points) {
-//         const convexHull = new GrahamScan();
-//         points.forEach(point => {
-//             convexHull.addPoint(point.latitude, point.longitude);
-//         });
-//         const hull = convexHull.getHull().map(h => ({ latitude: h.x, longitude: h.y }));
-//         setHullPoints(hull);
-//     }
+    // Updates visited polygon
+    function updateHull(points) {
+        const convexHull = new GrahamScan();
+        points.forEach(point => {
+            convexHull.addPoint(point.latitude, point.longitude);
+        });
+        const hull = convexHull.getHull().map(h => ({ latitude: h.x, longitude: h.y }));
+        setHullPoints(hull);
+    }
 
     function renderMarkers() {
         return places.map((place, index) => (
@@ -249,13 +249,13 @@ export default function HomeScreen() {
     );
 }
 
-// const styles = StyleSheet.create({
-//     container: {
-//         ...StyleSheet.absoluteFillObject,
-//         flex: 1,  // Make the view 100% of screen
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//     },
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        flex: 1,  // Make the view 100% of screen
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 
     map: {
         ...StyleSheet.absoluteFillObject
